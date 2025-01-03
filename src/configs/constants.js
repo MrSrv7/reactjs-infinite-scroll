de20293d17489ba0c430fac05048356af946b76d
Sri Vineeth <mrsrv7d@gmail.com>
2025-01-03 13:32:02 +0530

    [feat] Add App level constants

---
+++ src/configs/constants.js
@@ -0,0 +1,4 @@
+export const API_BASE_URL = 'https://jsonplaceholder.typicode.com';
+export const COMMENTS_PER_PAGE = 5;
+export const SIMULATED_DELAY_MS = 800;
+export const ROBOHASH_URL = 'https://robohash.org';
