e07ef8df331c26d69e19eb0e6ce0234fb254138d
Sri Vineeth <mrsrv7d@gmail.com>
2025-01-03 15:08:45 +0530

    [refactor] Update App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
+import { CommentsList } from "./components";
+
 const App = () => (
-  <section>
+  <section className="flex flex-col items-center justify-center h-screen">
     <h1>Infinite Scroll</h1>
+    <div className="mt-8 h-96 overflow-y-scroll w-1/2">
+      <CommentsList />
+    </div>
   </section>
 );
 
